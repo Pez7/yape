1c40b6846e8e1323c1ecb535c3b75ae843702d5e
Maria Paz <maria.pazrojasc@gmail.com>
2017-08-22 23:55:32 -0300

    validacion pantalla 4

---
+++ assets/js/pantalla4.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	var nombre = $('#name').val();
+	var email = $('#email').val();
+	var password = $('#pass').val();
+	var vacio = " ";
+
+	if (nombre == vacio) {
+		false;
+	}else if (email == vacio) {
+		false;
+	}else if (password == vacio && password > 7) {
+		false;
+	}
+
+	if (nombre == true && email == true && password == true) {
+		$('#crear').removeClass('disabled');
+		$('#crear').addClass('correct');
+	}
+
+
+	localStorage.getItem(nombre);
+	localStorage.getItem(email);
+	localStorage.getItem(password);
+});

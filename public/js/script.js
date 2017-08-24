2e6544414edf45487e9944a34f2366032efe4c4f
Anayn Pavez <nanyn.pavez@gmail.com>
2017-08-24 13:41:12 -0300

    validaciones de pantalla seis

--- public/js/script.js
+++ public/js/script.js
@@ -20392,9 +20392,57 @@ $(document).ready(function(){
 	var mes = $('#month').val();
 	var anio = $('#year').val();
 
+	console.log(tarjeta);
+
+	$(tarjeta).change(function(){
+		validarTarjeta();
+	});
+	$(mes).change(function() {
+		validarMes();
+	});
+	$(anio).change(function() {
+		validarAnio();
+	});
+	
+	function validarTarjeta(){
+		if (tarjeta.length != 16 || isNaN(tarjeta) ){
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+	function validarMes(){
+		if(mes.length != 2 || isNaN(mes)){
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+	function validarAnio(){
+		if(anio.length != 4 || isNaN(anio)){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	if(validarTarjeta && validarMes && validarAnio){
+		return true;
+		$('#continuar').removeClass('disabled');
+	}else{
+		return false;
+		$('#continuar').addClass('disabled');
+	}
+	$('#continuar').click(function() {
+		window.location.href = "pantalla6-2.html";
+	});
 	
 
 });
+
+
 $(document).ready(function() {
     var telefono = "";
 
